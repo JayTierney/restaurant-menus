7f30e3b5a7b7246f3eb140155efa7a72ba200bed
James Tierney <james.tierney384@gmail.com>
2022-08-10 12:28:52 +0100

    tables added

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
-// TODO - connect to db via sequelize
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: './data.sqlite'
+})
 
-module.exports = {
-    sequelize
-};
+module.exports = { db };
